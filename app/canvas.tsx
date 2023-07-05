1feb338da31b4d26f28739e2d1389af6c7c8410b
Yusuke Kawatsu <meg.mog.mog1965@gmail.com>
2023-07-05 12:09:39 +0900

    fix eslint.

--- app/canvas.tsx
+++ app/canvas.tsx
@@ -1,235 +1,236 @@
 'use client'
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect, useCallback } from "react"
 
 // constants.
-const DOT_LENGTH: number = 20;
-const MAX_WIDTH = 12;
-const MAX_HEIGHT = 20;
+const DOT_LENGTH: number = 20
+const MAX_WIDTH = 12
+const MAX_HEIGHT = 20
 
 // react component.
 export default function Canvas() {
-  const board = new Board();
-  let intervalId: number;
-
-  board.post();
-
-  // initializer.
+  // initializer & finalizer.
   useEffect(() => {
-    document.addEventListener("keydown", onKeyDown(board), false);
-    intervalId = runDaemon(board);
-  }, []);
+    const board = new Board()
 
-  // finalizer.
-  useEffect(() => {
+    document.addEventListener("keydown", onKeyDown(board), false)
+    const stopDaemon = runDaemon(board)
+
     return () => {
-      document.removeEventListener("keydown", onKeyDown(board), false);
-      clearInterval(intervalId);
-    };
-  }, []);
+      document.removeEventListener("keydown", onKeyDown(board), false)
+      stopDaemon()
+    }
+  }, [])
 
   return (
     <canvas id="canvas" width={DOT_LENGTH * MAX_WIDTH} height={DOT_LENGTH * MAX_HEIGHT}></canvas>
   )
 }
 
-function onKeyDown(board: Board) {
+function onKeyDown(board: Board): (event: KeyboardEvent) => void {
+  const actions: { [key: number]: () => void } = {
+    37: () => board.left(),
+    39: () => board.right(),
+    40: () => board.down(),
+    32: () => board.rotate(),
+  }
+
   return (event: KeyboardEvent) => {
-    if (event.keyCode === 37) {
-      board.left();
-    } else if (event.keyCode === 39) {
-      board.right();
-    } else if (event.keyCode === 40) {
-      board.down();
-    } else if (event.keyCode === 32) {
-      board.rotate();
+    const action = actions[event.keyCode]
+    if (!action) {
+      return
     }
 
-    drawBoard(board);
-  };
+    action()
+    drawBoard(board)
+  }
 }
 
-function runDaemon(board: Board) : number {
+function runDaemon(board: Board) : () => void {
   // init.
-  const context = getContext();
-  context.translate(0, MAX_HEIGHT * DOT_LENGTH);
-  context.scale(1, -1);
+  const context = getContext()
+  context.translate(0, MAX_HEIGHT * DOT_LENGTH)
+  context.scale(1, -1)
 
   const update = () => {
-    board.post();
-    board.down();
+    board.post()
+    board.down()
 
-    drawBoard(board);
-  };
+    drawBoard(board)
+  }
 
-  // @ts-ignore
-  return setInterval(update, 1000);
+  // run it.
+  const intervalId = setInterval(update, 1000)
+
+  // return function to stop daemon.
+  return () => clearInterval(intervalId)
 }
 
 function rand(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 function drawBoard(board: Board) {
-  const blocks = board.active ? [board.active, board.blocks] : [board.blocks];
-  const context = getContext();
+  const blocks = board.active ? [board.active, board.blocks] : [board.blocks]
+  const context = getContext()
 
-  context.clearRect(0, 0, MAX_WIDTH * DOT_LENGTH, MAX_HEIGHT * DOT_LENGTH);
+  context.clearRect(0, 0, MAX_WIDTH * DOT_LENGTH, MAX_HEIGHT * DOT_LENGTH)
 
   for (const block of blocks) {
     for (const point of block.points) {
-      const offsetX = (block.offset.x + point.x) * DOT_LENGTH;
-      const offsetY = (block.offset.y + point.y) * DOT_LENGTH;
+      const offsetX = (block.offset.x + point.x) * DOT_LENGTH
+      const offsetY = (block.offset.y + point.y) * DOT_LENGTH
 
-      context.fillStyle = 'green';
-      context.fillRect(offsetX, offsetY, DOT_LENGTH, DOT_LENGTH);
+      context.fillStyle = 'green'
+      context.fillRect(offsetX, offsetY, DOT_LENGTH, DOT_LENGTH)
     }
   }
 }
 
 function getContext(): CanvasRenderingContext2D {
-  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-  const context = canvas!.getContext('2d');
+  const canvas = document.getElementById('canvas') as HTMLCanvasElement
+  const context = canvas!.getContext('2d')
 
-  return context!;
+  return context!
 }
 
 class Board {
-  blocks: Block;
-  active?: Block;
+  blocks: Block
+  active?: Block
 
   constructor() {
-    this.blocks = new Block([], new Point(0, 0));
-    this.active = undefined;
+    this.blocks = new Block([], new Point(0, 0))
+    this.active = undefined
   }
 
   post() {
     if (this.active) {
-      return;
+      return
     }
 
-    const offset = new Point(Math.floor(MAX_WIDTH) / 2, MAX_HEIGHT - 1);
-    const block = new Block([new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(1, 1)], offset);
+    const offset = new Point(Math.floor(MAX_WIDTH) / 2, MAX_HEIGHT - 1)
+    const block = new Block([new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(1, 1)], offset)
 
-    this.active = block;
+    this.active = block
   }
 
   down() {
     if (!this.active) {
-      return;
+      return
     }
 
-    const offset = new Point(this.active.offset.x, this.active.offset.y - 1);
-    const newBlock = new Block(this.active.points, offset);
+    const offset = new Point(this.active.offset.x, this.active.offset.y - 1)
+    const newBlock = new Block(this.active.points, offset)
 
     if (this.hasCollision(newBlock)) {
-      this.blocks = this.blocks.merge(this.active);
-      this.active = undefined;
+      this.blocks = this.blocks.merge(this.active)
+      this.active = undefined
 
-      this.blocks = this.eraseLine(this.blocks);
+      this.blocks = this.eraseLines(this.blocks)
 
-      return;
+      return
     }
 
-    this.active = newBlock;
+    this.active = newBlock
   }
 
   left() {
     if (!this.active) {
-      return;
+      return
     }
 
-    const offset = new Point(this.active.offset.x - 1, this.active.offset.y);
-    const newBlock = new Block(this.active.points, offset);
+    const offset = new Point(this.active.offset.x - 1, this.active.offset.y)
+    const newBlock = new Block(this.active.points, offset)
 
     if (this.hasCollision(newBlock)) {
-      return;
+      return
     }
 
-    this.active = newBlock;
+    this.active = newBlock
   }
 
   right() {
     if (!this.active) {
-      return;
+      return
     }
 
-    const offset = new Point(this.active.offset.x + 1, this.active.offset.y);
-    const newBlock = new Block(this.active.points, offset);
+    const offset = new Point(this.active.offset.x + 1, this.active.offset.y)
+    const newBlock = new Block(this.active.points, offset)
 
     if (this.hasCollision(newBlock)) {
-      return;
+      return
     }
 
-    this.active = newBlock;
+    this.active = newBlock
   }
 
   rotate() {
     // not implemented yet.
-    return;
+    return
   }
 
   hasCollision(block: Block): boolean {
-    return !block.is_valid_offset() || this.blocks.has_collision(block);
+    return !block.isValidOffset() || this.blocks.hasCollision(block)
   }
 
-  eraseLine(blocks: Block): Block {
+  eraseLines(blocks: Block): Block {
     // validate for each lines.
     for (let i = 0; i < MAX_HEIGHT; i++) {
-      const line = blocks.points.filter((point) => point.y === i);
+      const line = blocks.points.filter((point) => point.y === i)
       
       if (line.length === MAX_WIDTH) {
-        let points = blocks.points.filter((point) => point.y !== i);
-        points = points.map((point) => point.y > i ? new Point(point.x, point.y - 1) : point);
-        blocks = new Block(points, blocks.offset);
+        let points = blocks.points.filter((point) => point.y !== i)
+        points = points.map((point) => point.y > i ? new Point(point.x, point.y - 1) : point)
+        blocks = new Block(points, blocks.offset)
 
         // call recursively.
-        return this.eraseLine(blocks);
+        return this.eraseLines(blocks)
       }
     }
 
-    return blocks;
+    return blocks
   }
 }
 
 class Block {
-  points: Point[];
-  offset: Point;
+  points: Point[]
+  offset: Point
 
   constructor(points: Point[], offset: Point) {
-    this.points = points;
-    this.offset = offset;
+    this.points = points
+    this.offset = offset
   }
 
-  has_collision(block: Block): boolean {
-    const absPointsThis = this.points.map((point) => new Point(this.offset.x + point.x, this.offset.y + point.y));
-    const absPointsOthers = block.points.map((point) => new Point(block.offset.x + point.x, block.offset.y + point.y));
+  hasCollision(block: Block): boolean {
+    const absPointsThis = this.points.map((point) => new Point(this.offset.x + point.x, this.offset.y + point.y))
+    const absPointsOthers = block.points.map((point) => new Point(block.offset.x + point.x, block.offset.y + point.y))
 
-    return absPointsThis.some((point) => absPointsOthers.some((other) => point.x === other.x && point.y === other.y));
+    return absPointsThis.some((point) => absPointsOthers.some((other) => point.x === other.x && point.y === other.y))
   }
 
-  is_valid_offset(): boolean {
-    const absPointsThis = this.points.map((point) => new Point(this.offset.x + point.x, this.offset.y + point.y));
-    const ret = absPointsThis.every((point) => point.x >= 0 && point.x < MAX_WIDTH && point.y >= 0 && point.y < MAX_HEIGHT);
+  isValidOffset(): boolean {
+    const absPointsThis = this.points.map((point) => new Point(this.offset.x + point.x, this.offset.y + point.y))
 
-    return ret;
+    return absPointsThis.every((point) => point.x >= 0 && point.x < MAX_WIDTH && point.y >= 0 && point.y < MAX_HEIGHT)
   }
 
   merge(block: Block): Block {
-    const absPointsOthers = block.points.map((point) => new Point(block.offset.x + point.x, block.offset.y + point.y));
-    const points = this.points.concat(absPointsOthers);
-    const offset = new Point(0, 0);
+    const absPointsThis = this.points.map((point) => new Point(this.offset.x + point.x, this.offset.y + point.y))
+    const absPointsOthers = block.points.map((point) => new Point(block.offset.x + point.x, block.offset.y + point.y))
 
-    return new Block(points, offset);
+    const points = absPointsThis.concat(absPointsOthers)
+    const offset = new Point(0, 0)
+
+    return new Block(points, offset)
   }
 }
 
 class Point {
-  x: number;
-  y: number;
+  x: number
+  y: number
 
   constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
+    this.x = x
+    this.y = y
   }
 }
 
